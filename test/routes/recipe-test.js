231d6ec3a0e9216c81ab9a250a9205b9d911b146
Siobhan O Mahony <siobhanomahony1994@hotmail.com>
2017-11-01 13:39:08 +0000

    API test setup

---
+++ test/routes/recipe-test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../bin/www');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+var _ = require('lodash' );
+
+describe('Donations', function (){
+    // TODO
+});
