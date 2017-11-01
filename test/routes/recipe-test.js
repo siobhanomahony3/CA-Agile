a70da329a000f599c6e52864da3409935791e209
Siobhan O Mahony <siobhanomahony1994@hotmail.com>
2017-11-01 13:48:44 +0000

    Get recipe test complete

--- test/routes/recipe-test.js
+++ test/routes/recipe-test.js
@@ -6,6 +6,22 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 var _ = require('lodash' );
 
-describe('Donations', function (){
+describe('Recipe', function (){
     // TODO
+});
+
+describe('Recipe', function (){
+    describe('GET /recipe', function () {
+        it('should return all the recipes in an database', function(done) {
+            chai.request(server)
+                .get('/recipe')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(2);
+                    done();
+                });
+        });
+    });
+
 });
