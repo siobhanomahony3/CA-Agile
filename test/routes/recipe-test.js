fdef0ba04c4211148602651dfb9982d4d43b5517
Siobhan O Mahony <siobhanomahony1994@hotmail.com>
2017-12-12 23:10:49 +0000

    Trialing Travis test execution 9

--- test/routes/recipe-test.js
+++ test/routes/recipe-test.js
@@ -6,16 +6,16 @@ var recipe = require('../../models/recipe');
 chai.use(chaiHttp);
 var _ = require('lodash' );
 
-    var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost:27017/recipe');
-    var db = mongoose.connection;
-
-    db.on('error', function (err) {
-        console.log('connection error', err);
-    });
-    db.once('open', function () {
-        console.log('connected to database');
-    });
+    // var mongoose = require('mongoose');
+    // mongoose.connect('mongodb://localhost:27017/recipe');
+    // var db = mongoose.connection;
+    //
+    // db.on('error', function (err) {
+    //     console.log('connection error', err);
+    // });
+    // db.once('open', function () {
+    //     console.log('connected to database');
+    // });
 
     describe('Recipe', function (){
         beforeEach(function (done) {
