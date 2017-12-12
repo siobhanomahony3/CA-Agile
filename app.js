e5d075e1bb732ed05b182884d544157f27101f99
Siobhan O Mahony <siobhanomahony1994@hotmail.com>
2017-12-12 03:33:52 +0000

    Add build tasks to Webpack

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.use('/', index);
 app.use('/users', users);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     debug: true,
     devtool: 'source-map',
@@ -14,6 +15,11 @@ module.exports = {
         publicPath: 'http://localhost:3000/',
         filename: 'bundle.js'
     },
+    plugins: [
+        new CleanWebpackPlugin(['build']),
+        new HtmlWebpackPlugin({ inject: 'head',
+            template: __dirname + "/public/index.tmpl.html"})
+    ],
     module: {
         loaders: [
             { test: /\.js$/,
